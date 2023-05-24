fe14679d3d148a3c46d4de5c3c541ed6bacc29b9
Pucas <andre.pucas.santos@gmail.com>
2023-05-24 20:16:36 +0100

    Add item panel transition in shop

--- 2d-shopkeeper/Assets/Scripts/UI/Panels/UIPanelShopkeeper.cs
+++ 2d-shopkeeper/Assets/Scripts/UI/Panels/UIPanelShopkeeper.cs
@@ -12,15 +12,49 @@ public class UIPanelShopkeeper : UIPanelAbstract
     
     // --- VARIABLES -----------------------------------------------------------
 
+    [SerializeField] private GameObject[] _itemPanels;
+
+    [Header("BUTTONS")]
+    [SerializeField] private GameObject _nextButton;
+    [SerializeField] private GameObject _previousButton;
+
+    private int _currentItemPanel;
+
     // --- METHODS -------------------------------------------------------------
     
     // Shows panel.
-    public new void Open(float p_fade = 0) => base.Open(p_fade);
+    public new void Open(float p_fade = 0)
+    {
+        foreach(GameObject f_panel in _itemPanels)
+            f_panel.SetActive(false);
+        
+        _itemPanels[0].SetActive(true);
+        _currentItemPanel = 0;
+        UpdateButtons();
+
+        base.Open(p_fade);
+    }
 
     // Hides panel.
     public new void Close(float p_fade = 0) => base.Close(p_fade);
 
     public void Done() => OnDone?.Invoke();
+
+    public void NextItemPanel(int p_add)
+    {
+        _itemPanels[_currentItemPanel].SetActive(false);
+
+        _currentItemPanel += p_add;
+        _itemPanels[_currentItemPanel].SetActive(true);
+
+        UpdateButtons();
+    }
+
+    private void UpdateButtons()
+    {
+        _previousButton.SetActive(_currentItemPanel != 0);
+        _nextButton.SetActive(_currentItemPanel != _itemPanels.Length-1);
+    }
 
     public void DeselectAll() => EventSystem.current.SetSelectedGameObject(null);
 }
